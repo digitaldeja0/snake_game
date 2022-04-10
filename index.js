611a45743c5a97d387b31d06688e206fd689abac
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2022-04-09 22:14:54 -0400

    Add files

---
+++ index.js
@@ -0,0 +1,140 @@
+//javascript
+
+const grid = document.querySelector(".grid")
+const startButton = document.getElementById("start")
+const scoreDisplay = document.getElementById("score")
+let squares = []
+let currentSnake = [2, 1, 0]
+let direction = 1
+const width = 10
+let appleIndex = 0
+let score = 0
+let intervalTime = 800
+let speed = 0.5
+let timerId = 0
+
+function createGrid() {
+
+  for (let i = 0; i < width * width; i++) {
+
+    const square = document.createElement("div")
+
+    square.classList.add('square')
+
+    grid.appendChild(square)
+
+    squares.push(square)
+  }
+}
+
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add("snake"))
+
+function startGame(){
+
+  currentSnake.forEach(index => squares[index].classList.remove("snake"))
+  squares[appleIndex].classList.remove('apple')
+
+  clearInterval(timerId)
+  currentSnake = [2,1,0]
+  score = 0
+  scoreDisplay.textContent = score
+  direction = 1
+  intervalTime = 800
+  generateApple()
+  timerId = setInterval(move, intervalTime)
+}
+
+
+function move() {
+
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+
+  )
+
+  return clearInterval(timerId)
+
+  const tail = currentSnake.pop()
+  squares[tail].classList.remove("snake")
+  currentSnake.unshift(currentSnake[0] + direction)
+
+  if (squares[currentSnake[0]].classList.contains('apple')) {
+
+    //remove the class apple
+
+    squares[currentSnake[0]].classList.remove("apple")
+
+    //grow snake by adding class of snake to it
+
+    squares[tail].classList.add("snake")
+
+    //grow our snake array
+
+    currentSnake.push(tail)
+
+    //generate a new appleIndex
+
+    generateApple()
+
+    //add one to the score
+
+    score++
+
+    //display our score
+
+
+    scoreDisplay.textContent = score
+
+    //speed up the snake
+
+    clearInterval(timerId)
+
+    intervalTime = intervalTime * speed
+
+    timerId = setInterval(move, intervalTime)
+
+
+  }
+
+  squares[currentSnake[0]].classList.add("snake")
+
+}
+
+
+function generateApple() {
+  do {
+    appleIndex = Math.floor(Math.random() * squares.length)
+  } while (squares[appleIndex].classList.contains('snake'))
+  squares[appleIndex].classList.add('apple')
+}
+
+generateApple()
+
+function control(e) {
+  if (e.key === "ArrowRight") {
+    console.log("right arrow")
+    direction = 1
+  } else if (e.key === "ArrowUp") {
+    console.log("up-arrow")
+    direction = -width
+  } else if (e.key === "ArrowLeft") {
+    console.log("left arrow")
+    direction = -1
+  } else if (e.key === "ArrowDown") {
+    console.log("down arrow")
+    direction = +width
+  } else {
+    console.log("invalid button pressed 🙃")
+  }
+
+}
+
+document.addEventListener("keyup", control)
+
+startButton.addEventListener("click", startGame)
